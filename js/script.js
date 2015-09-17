cbf24b55796b31835b948f94e95ac93e2a3c8072
Robin Hamill <hi@robinhamill.ca>
2015-09-16 20:31:44 -0400

    working out calc functions

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
+// Pseudo Code
+// 1. when user clicks on a number button, add it to a string, and the screen
+// 2. When pressing an operator, add it to the the string
+// 3. When pressing 'C' clear the screen, and the string.
+// 4. When pressing the '=', perform the calculation and present on the screen
+
 console.log('Welcome Robin');
 var app = [];
+var formulaString = "";
 
-app.calculation = function() {
+app.calculation = function(x) {
 	$(".btn").on("click", function() {
 		var number = $(this).attr("value");
-		console.log(number);
+		formulaString += number;
+		$(".screen").text(formulaString);
+		console.log(formulaString);
 		console.log("click");
 	});
-};
-app.calculation();
+	$(".btnEq").on("click", function() {
+		formulaString.eval();
+		$(".screen").empty().text(formulaString);
+	});
+}(); 
